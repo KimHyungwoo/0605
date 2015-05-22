77ab51a3980995f8733ec7289df4d0013ecafeaa
301 <301@223.194.169.102>
2015-05-22 15:54:10 +0900

    0522

---
+++ board.sql
@@ -0,0 +1,25 @@
+create table board
+(
+	num 		number			 primary key,
+	name 		varchar2(10),
+	title 		varchar2(100),
+	content 	varchar2(1000),
+	hits 		number 			 default 0,
+	wdate 						 date default sysdate,
+	attachment  varchar2(50)
+)
+
+-- 시퀀스 생성
+create sequence board_seq
+
+insert into board
+values(board_seq.nextval,'홍길동','제목','게시판',default,default,'첨부')
+ 
+select * from board
+
+delete board
+
+drop table board
+
+
+
---
+++ src/board/dao/BoardDAO.java
@@ -0,0 +1,36 @@
+package board.dao;
+
+import java.util.List;
+import board.dto.Board;
+
+public class BoardDAO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int insertBoard(Board board) {
+		return 0;	
+	}
+	
+	public List<Board> selectAllBoards() {
+		return null;
+	}
+	
+	public Board seletOnBoardByNum(int num) {
+		return null;
+	}
+	
+	public void updateHits(int num) {
+		
+	}
+	
+	public void updateBoard(Board board) {
+		
+	}
+	
+	public void deleteBoard(int num) {
+		
+	}
+}
